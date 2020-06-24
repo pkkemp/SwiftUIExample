c7f27f108d1fcff10f7bceaf3f4d4d07b01fcbb3
Preston Kemp <preston@prestonkemp.com>
2020-06-23 22:51:18 -0500

    initial setup

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Preston Kemp on 6/23/20.
+//  Copyright © 2020 Preston Kemp. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Preston!")
+            .font(.title)
+            .foregroundColor(.purple)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
